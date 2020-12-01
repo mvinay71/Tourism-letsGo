dd7f9e5d9c0509654efadb8b60b10abee9ba7f91
vinay kumar maurya <v7naym@gmail.com>
2020-12-01 19:15:31 +0530

    half done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlogsComponent } from './blogs/blogs.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:"", pathMatch: "full",redirectTo:"/home"},
+  {path:"home",component:HomeComponent},
+  {path:"blogs",component:BlogsComponent},
+  {path:"contact",component:ContactComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
